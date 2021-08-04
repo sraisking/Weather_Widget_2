7e3715b9021febbbaaa4681e47654edb5a67c147
Shahrukh.akhter486@gmail.com <Shahrukh.akhter486@gmail.com>
2021-08-04 23:18:02 +0530

    Refactored

--- weatherwidget2/src/sections/DayWiseForecast.js
+++ weatherwidget2/src/sections/DayWiseForecast.js
@@ -1,35 +1,46 @@
-import { Grid, Typography } from '@material-ui/core'
+import { makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
 import { getDay } from '../Utils'
+const useStyles = makeStyles({
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center'
+    },
+    textContainer: {
+        display: 'flex'
+    }
+});
 const DayWiseForeCast = ({ weatherForecast }) => {
+    const classes = useStyles();
     return (
         <div style={{ padding: '8px' }}>
-            <Grid container direction="column" alignContent="center">
-                <Grid item>
+            <div className={classes.content}>
+                <div>
                     <Typography>
                         {getDay(weatherForecast.dt)}
                     </Typography>
-                </Grid>
-                <Grid item>
+                </div>
+                <div>
                     <img src={`http://openweathermap.org/img/wn/${weatherForecast.weather[0].icon}@2x.png`} alt="ICON" />
-                </Grid>
-                <Grid item>
-                    <Grid container spacing={2}>
-                        <Grid item>
+                </div>
+                <div>
+                    <div className={classes.textContainer}>
+                        <div>
                             <Typography variant="subtitle1">
                                 {weatherForecast.temp.min}
                             </Typography>
-                        </Grid>
-                        <Grid item>
+                        </div>
+                        <div>
                             <Typography variant="h6">
                                 {weatherForecast.temp.max}
                             </Typography>
-                        </Grid>
-                    </Grid>
+                        </div>
+                    </div>
 
 
-                </Grid>
-            </Grid>
+                </div>
+            </div>
         </div>
     )
 }
